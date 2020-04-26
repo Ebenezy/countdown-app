8bb6c2ee542da03fe52d3c2df9849ad50fb9dc66
Ebenezer Ndukwe <ebnettech@gmail.com>
2020-04-26 03:40:20 +0100

    Countdown React Native App

--- App.tsx
+++ App.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { YellowBox } from "react-native";
+import EventList from "./EventList";
+import EventForm from "./EventForm";
+// import { StackNavigator, } from 'react-navigation';
+import 'react-native-gesture-handler';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+YellowBox.ignoreWarnings([
+  "Warning: componentWillMount is deprecated",
+  "Warning: componentWillReceiveProps is deprecated",
+]);
+// export default function App() {
+// export default class App extends React.Component {
+//   render() {
+//     return <EventList />;
+//   }
+// }
+
+
+const Stack = createStackNavigator();
+
+// function MyStack() {
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="list"
+          component={EventList}
+          options={{title: 'Your events'}}
+        />
+        <Stack.Screen 
+        name="form" 
+        component={EventForm} 
+        options={{title: 'Add an events'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default StackNavigator({
+//   list: {
+//     screen: EventList,
+//     navigationOptions: () => ({
+//       title: 'Your events',
+//     }),
+//   },
+//   form: {
+//     screen: EventForm,
+//     navigationOptions: () => ({
+//       title: 'Add an event',
+//     }),
+//   },
+// });
+
